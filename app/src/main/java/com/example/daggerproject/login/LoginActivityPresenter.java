348d02851e597bcafede90f0c1798c455800f2ae
mohamadeslami <mohammadeslami1379@yahoo.com>
2020-09-23 23:55:18 +0330

    presenter tested with null user

--- app/src/main/java/com/example/daggerproject/login/LoginActivityPresenter.java
+++ app/src/main/java/com/example/daggerproject/login/LoginActivityPresenter.java
@@ -40,7 +40,7 @@ public class LoginActivityPresenter implements LoginActivityMVP.Presenter {
         User user = model.getUser();
         if(user == null){
             if(view != null){
-                view.showUserNotAvailable();
+                //view.showUserNotAvailable();
             }
         }else{
             if(view != null){
---
+++ app/src/test/java/com/example/daggerproject/PresenterTests.java
@@ -0,0 +1,41 @@
+package com.example.daggerproject;
+
+import com.example.daggerproject.login.LoginActivityMVP;
+import com.example.daggerproject.login.LoginActivityPresenter;
+import com.example.daggerproject.login.User;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+public class PresenterTests {
+
+    LoginActivityMVP.Model mockLoginModel;
+    LoginActivityMVP.View mockView;
+    LoginActivityMVP.Presenter presenter;
+    User user;
+
+    @Before
+    public void setup(){
+        mockLoginModel = mock(LoginActivityMVP.Model.class);
+        user = new User("FOX" , "Rider");
+        //when(mockLoginModel.getUser()).thenReturn(user);
+        mockView = mock(LoginActivityMVP.View.class);
+        presenter = new LoginActivityPresenter(mockLoginModel);
+        presenter.setView(mockView);
+
+    }
+
+
+    @Test
+    public void noInteractionWithView(){
+        presenter.getCurrentUser();
+        verifyZeroInteractions(mockView);
+    }
+
+
+
+}
